064d779a47f29a06a3b7ef1c7c7c33feb7cf795a
HoangTung17 <77482764+HoangTung17@users.noreply.github.com>
2022-03-09 13:32:58 +0700

    date

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const yourDate = new Date("2001-10-13T22:53:26"),
+const yourDate = new Date("2021-08-30T22:53:26"),
 music = ['ido', 'noinaycoanh', 'nguoiamphu'];
 
 document.addEventListener('DOMContentLoaded', function(){
